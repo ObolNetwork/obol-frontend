ef18c590b732d3ebff54e9758e685998c067d34d
George Weiler <georgejweiler@gmail.com>
2022-02-04 10:39:24 -0700

    Add chakra, chakra theme, component.v2 folder, header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Header from "../components.v2/Header"
+import theme from "../theme"
 
-// @ts-ignore
-export default function IndexPage({ data }) {
+export default function IndexPage() {
   return (
-    <Layout>
-      <SEO title="Obol Network" />
-    </Layout>
+    <ChakraProvider theme={theme}>
+      <Header />
+      <Layout>
+        <SEO title="Obol Network" />
+      </Layout>
+    </ChakraProvider>
   )
 }
-
-
