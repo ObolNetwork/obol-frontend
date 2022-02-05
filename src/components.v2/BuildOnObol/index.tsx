042863a174aca5911e8b72d0f90510630acfa92a
George Weiler <georgejweiler@gmail.com>
2022-02-04 17:51:16 -0700

    build the build-with-obol section

---
+++ src/components.v2/BuildOnObol/index.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { Box, Image } from "@chakra-ui/react"
+import obolHexes from "../../images/obol-hexes.png"
+import obolHexesMobile from "../../images/obol-hexes-mobile.png"
+import obolSwivel from "../../images/obol-swivel.png"
+import { useMediaQuery } from "@material-ui/core"
+import DotsDivider from "../DotsDivider"
+
+const BuildOnObol = () => {
+  const isLargerThan900 = useMediaQuery("(min-width: 900px)")
+
+  return (
+    <Box
+      bg="transparent"
+      p="36px"
+      bgImage={obolSwivel}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="1100px 600px"
+    >
+      <Image
+        src={isLargerThan900 ? obolHexes : obolHexesMobile}
+        maxW="750px"
+        margin="auto"
+      />
+    </Box>
+  )
+}
+
+export default BuildOnObol
