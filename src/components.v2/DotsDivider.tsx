4dfa24ce8566fdf38ca7dd503fea4f3cf32b7e63
George Weiler <georgejweiler@gmail.com>
2022-02-04 17:51:16 -0700

    build the build-with-obol section

---
+++ src/components.v2/DotsDivider.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Circle, HStack } from "@chakra-ui/react"
+
+const dotColors = [
+  "#3B93BE",
+  "#51AAC6",
+  "#6FD1D7",
+  "#81BCB9",
+  "#6FD1D7",
+  "#51AAC6",
+  "#3B93BE",
+]
+
+const DotsDivider = () => {
+  return (
+    <HStack spacing={4} my={12} justifyContent="center">
+      {dotColors.map((color, i) => (
+        <Circle size="10px" bg={color} key={`${color - i}`} />
+      ))}
+    </HStack>
+  )
+}
+
+export default DotsDivider
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,17 @@ import { Box, ChakraProvider } from "@chakra-ui/react"
 import Header from "../components.v2/Header"
 import theme from "../theme"
 import Hero from "../components.v2/Hero"
+import BuildOnObol from "../components.v2/BuildOnObol"
+import DotsDivider from "../components.v2/DotsDivider"
 
 const IndexPage = () => {
   return (
     <Box bg="obol.black" h="100vh">
       <Header />
       <Hero />
+      <DotsDivider />
+      <BuildOnObol />
+      <DotsDivider />
     </Box>
   )
 }
