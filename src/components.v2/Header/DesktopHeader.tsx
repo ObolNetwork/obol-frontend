080db274aedabfe77d99b023c0be3322d3b72fa1
George Weiler <georgejweiler@gmail.com>
2022-02-07 12:47:09 -0700

    Add responsive styles to navbar

---
+++ src/components.v2/Header/DesktopHeader.tsx
@@ -0,0 +1,55 @@
+import React, { FC } from "react"
+import { Box, Button, HStack, Icon, Image } from "@chakra-ui/react"
+import ObolBrand from "../../images/obolnetwork.png"
+import HeaderLink, { HeaderLinkProps } from "./HeaderLink"
+import { FaDiscord, GiHamburgerMenu } from "react-icons/all"
+
+const DesktopHeader: FC<{
+  headerLinks: HeaderLinkProps[]
+  onOpen: () => void
+}> = ({ headerLinks, onOpen }) => {
+  return (
+    <Box
+      bg="obol.black"
+      p={4}
+      height="60px"
+      position="relative"
+      display="flex"
+      justifyContent="flex-end"
+      borderBottom="2px solid"
+      borderColor="obol.lightGreen"
+    >
+      <Image
+        height="30px"
+        src={ObolBrand}
+        maxHeight="100%"
+        maxWidth="100%"
+        width="auto"
+        position="absolute"
+        top={0}
+        bottom={0}
+        left="24px"
+        margin="auto"
+      />
+      <HStack spacing={4}>
+        {headerLinks.map(link => (
+          <HeaderLink {...link} key={link.text} />
+        ))}
+
+        <Button leftIcon={<Icon fontSize="24px" as={FaDiscord} />} size="sm">
+          Join us
+        </Button>
+        <Icon
+          display={{ base: "inline", md: "none" }}
+          onClick={onOpen}
+          as={GiHamburgerMenu}
+          color="white"
+          boxSize="24px"
+          cursor="pointer"
+        />
+      </HStack>
+    </Box>
+  )
+}
+
+export default DesktopHeader
--- src/components.v2/Header/index.tsx
+++ src/components.v2/Header/index.tsx
@@ -1,9 +1,9 @@
 import React from "react"
-import HeaderLink, { HeaderLinkProps } from "./HeaderLink"
-import { Box, Button, HStack, Image, Icon } from "@chakra-ui/react"
-import { FaDiscord } from "react-icons/all"
-import ObolBrand from "../../images/obolnetwork.png"
+import { HeaderLinkProps } from "./HeaderLink"
+import { useDisclosure } from "@chakra-ui/react"
 import { Href } from "../../types"
+import MobileDrawer from "./MobileDrawer"
+import DesktopHeader from "./DesktopHeader"
 
 const headerLinks: HeaderLinkProps[] = [
   {
@@ -29,38 +29,13 @@ const headerLinks: HeaderLinkProps[] = [
 ]
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Box
-      bg="obol.black"
-      p={4}
-      height="60px"
-      position="relative"
-      display="flex"
-      justifyContent="flex-end"
-      borderBottom="2px solid"
-      borderColor="obol.lightGreen"
-    >
-      <Image
-        height="30px"
-        src={ObolBrand}
-        maxHeight="100%"
-        maxWidth="100%"
-        width="auto"
-        position="absolute"
-        top={0}
-        bottom={0}
-        left="24px"
-        margin="auto"
-      />
-      <HStack spacing={4}>
-        {headerLinks.map(link => (
-          <HeaderLink {...link} key={link.text} />
-        ))}
-        <Button leftIcon={<Icon fontSize="24px" as={FaDiscord} />} size="sm">
-          Join us
-        </Button>
-      </HStack>
-    </Box>
+    <>
+      <MobileDrawer {...{ isOpen, onClose, headerLinks }} />
+      <DesktopHeader {...{ onOpen, headerLinks }} />
+    </>
   )
 }
 
