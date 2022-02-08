79e1177af7edf0c9d66dd7179ce4a7e891fd4243
George Weiler <georgejweiler@gmail.com>
2022-02-07 18:07:58 -0700

    Build the supporters section, pausing to wait for more assets

--- src/pages/Landing/SupportedBy/index.tsx
+++ src/pages/Landing/SupportedBy/index.tsx
@@ -1,10 +1,87 @@
 import React, { FC } from "react"
-import { Box, Text } from "@chakra-ui/react"
+import {
+  Container,
+  Box,
+  Text,
+  Image,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react"
+import advancedBlockchain from "../../../images/sponsors/advanced-blockchain.png"
+import archetype from "../../../images/sponsors/archetype.png"
+import attestant from "../../../images/sponsors/attestant.png"
+import blockdaemon from "../../../images/sponsors/blockdaemon.png"
+import chorus from "../../../images/sponsors/chorus.png"
+import coinbaseVentures from "../../../images/sponsors/coinbase-ventures.png"
+import divergence from "../../../images/sponsors/divergence.png"
+import figment from "../../../images/sponsors/figment.png"
+import iosg from "../../../images/sponsors/iosg.png"
+import lao from "../../../images/sponsors/lao.png"
+import stableNode from "../../../images/sponsors/stable-node.png"
+import stakingFacilities from "../../../images/sponsors/staking-facilities.png"
+import yeildVentures from "../../../images/sponsors/yeild-ventures.png"
+import { Header4 } from "../../../components.v2/Typography"
+
+const sponsorLogos = [
+  { text: "Advanced Blockchain", logo: advancedBlockchain },
+  { text: "Archetype", logo: archetype },
+  { text: "Attestant", logo: attestant },
+  { text: "Blockdaemon", logo: blockdaemon },
+  { text: "Chorus", logo: chorus },
+  { text: "Coinbase Ventures", logo: coinbaseVentures },
+  { text: "Divergence", logo: divergence },
+  { text: "Digment", logo: figment },
+  { text: "IOSG", logo: iosg },
+  { text: "The LAO", logo: lao },
+  { text: "Stable Node", logo: stableNode },
+  { text: "Staking Facilities", logo: stakingFacilities },
+  { text: "Yeild Ventures", logo: yeildVentures },
+]
 
 const SupportedBy: FC = () => {
   return (
     <Box id="supporters">
-      <Text>Supported By</Text>
+      <Container maxW="6xl">
+        <Text>Supported By</Text>
+        <SimpleGrid minChildWidth="220px" spacing="20px" margin="auto">
+          {sponsorLogos.map(({ text, logo }) => (
+            <VStack
+              justifyContent="flex-end"
+              bg="obol.gradientDarkOpaque"
+              _hover={{
+                bg: "obol.gradientDark",
+              }}
+              transition="opacity 2.5s"
+              borderRadius="md"
+              pb="12px"
+              cursor="pointer"
+            >
+              <Box
+                w="250px"
+                h="150px"
+                textAlign="center"
+                display="table-cell"
+                verticalAlign="middle"
+                position="relative"
+              >
+                <Image
+                  src={logo}
+                  maxHeight="80%"
+                  maxWidth="80%"
+                  width="auto"
+                  position="absolute"
+                  top={0}
+                  bottom={0}
+                  left={0}
+                  right={0}
+                  margin="auto"
+                />
+              </Box>
+              <Header4>{text}</Header4>
+            </VStack>
+          ))}
+        </SimpleGrid>
+      </Container>
     </Box>
   )
 }
--- src/pages/Landing/TheTeam/index.tsx
+++ src/pages/Landing/TheTeam/index.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from "react"
 import { Box, VStack, SimpleGrid, Text } from "@chakra-ui/react"
 import TeamMemberCard from "./TeamMemberCard"
+import geoorge from "../../../images/geoorge.jpg"
 
 export interface TeamMember {
   name: string
   title: string
   twitter: string
   github: string
+  avatar: any
 }
 
 const teamMembers: TeamMember[] = [
@@ -15,12 +17,14 @@ const teamMembers: TeamMember[] = [
     title: "Project Lead & Founder",
     twitter: "https://twitter.com/StakeETH",
     github: "https://github.com/collinjmyers",
+    avatar: geoorge,
   },
   {
     name: "Oisin Kyne",
     title: "Head of Technology & Co-Founder",
     twitter: "https://twitter.com/OisinKyne",
     github: "https://github.com/OisinKyne",
+    avatar: geoorge,
   },
 
   {
@@ -28,6 +32,7 @@ const teamMembers: TeamMember[] = [
     title: "Dev Ops Lead",
     twitter: "https://twitter.com/eth2devops",
     github: "https://github.com/eth2devops",
+    avatar: geoorge,
   },
 
   {
@@ -35,6 +40,7 @@ const teamMembers: TeamMember[] = [
     title: "Tech Lead",
     twitter: "https://github.com/corverroos",
     github: "https://twitter.com/corverdev",
+    avatar: geoorge,
   },
 
   {
@@ -42,6 +48,7 @@ const teamMembers: TeamMember[] = [
     title: "Senior Software Engineer",
     twitter: "https://twitter.com/julesdesmit",
     github: "https://github.com/julesdesmit",
+    avatar: geoorge,
   },
 
   {
@@ -49,6 +56,7 @@ const teamMembers: TeamMember[] = [
     title: "Product Lead",
     twitter: "https://twitter.com/Battenfield",
     github: "https://github.com/Battenfield",
+    avatar: geoorge,
   },
 
   {
@@ -56,6 +64,7 @@ const teamMembers: TeamMember[] = [
     title: "Software Engineer",
     twitter: "https://twitter.com/dhruvbodani",
     github: "https://github.com/dB2510",
+    avatar: geoorge,
   },
 
   {
@@ -63,12 +72,14 @@ const teamMembers: TeamMember[] = [
     title: "Software Engineer",
     twitter: "https://twitter.com/xenowits",
     github: "https://github.com/xenowits",
+    avatar: geoorge,
   },
   {
     name: "Richard Patel",
     title: "Protocol Engineer",
     twitter: "https://twitter.com/terorie_dev",
     github: "https://github.com/terorie",
+    avatar: geoorge,
   },
 ]
 
@@ -78,7 +89,7 @@ const TheTeam: FC = () => {
       <Text>the team</Text>
       <Box
         mx={8}
-        bg="obol.gradientDark"
+        bg="obol.gradientDarkOpaque"
         p="24px"
         w="100%"
         maxWidth="998px"
@@ -86,7 +97,7 @@ const TheTeam: FC = () => {
       >
         <SimpleGrid minChildWidth="220px" spacing="110px">
           {teamMembers.map(member => (
-            <TeamMemberCard {...member} />
+            <TeamMemberCard {...member} key={member.name} />
           ))}
         </SimpleGrid>
       </Box>
